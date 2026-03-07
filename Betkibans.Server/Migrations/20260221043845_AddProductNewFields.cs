151c3ee47f635cb896562a770a9418cf1e538094
Bhumika Karki <karkibhumi015@gmail.com>
2026-03-07 15:27:28 +0545

    Feat: Add migration for new product fields

---
+++ Betkibans.Server/Migrations/20260221043845_AddProductNewFields.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Betkibans.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProductNewFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CareInstructions",
+                table: "Products",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CareWarnings",
+                table: "Products",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Color",
+                table: "Products",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CraftingTimeDays",
+                table: "Products",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "DiscountPrice",
+                table: "Products",
+                type: "numeric",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FinishType",
+                table: "Products",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CareInstructions",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "CareWarnings",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Color",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "CraftingTimeDays",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "DiscountPrice",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "FinishType",
+                table: "Products");
+        }
+    }
+}
