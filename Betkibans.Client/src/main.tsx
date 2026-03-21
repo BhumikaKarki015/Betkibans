37f982b19ca626b1eaad8915c8d1c7ec2c1a9f40
Bhumika Karki <karkibhumi015@gmail.com>
2026-03-21 20:57:38 +0545

    fix: import mobile.css

--- Betkibans.Client/src/main.tsx
+++ Betkibans.Client/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import 'bootstrap/dist/css/bootstrap.min.css'; 
 import './index.css';
+import './styles/mobile.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
