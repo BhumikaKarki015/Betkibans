49d2e813b9fd98e1f7aea8d8b08724fd4a0ba42c
Bhumika Karki <karkibhumi015@gmail.com>
2026-05-10 08:56:05 +0545

    fix: remove circular reference bloat in product query

--- Betkibans.Server/Controllers/ProductController.cs
+++ Betkibans.Server/Controllers/ProductController.cs
@@ -37,6 +37,7 @@ public class ProductController : ControllerBase
     )
     {
         var query = _context.Products
+            .AsNoTracking() 
             .Include(p => p.Category)
             .Include(p => p.ProductImages)
             .Include(p => p.ProductMaterials)
